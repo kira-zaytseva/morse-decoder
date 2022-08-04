c0e48a2702bc0f4a618ee07ff89b7da1048ea0e5
Kira Zaytseva <kirazaytseva@192.168.1.38>
2022-08-05 00:59:34 +0300

    feat: add morse decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = '';
+  let count = 0;
+  let countStar = 0;
+    let string1 = expr.split('').reduce((acc, rec, index) => {
+      let morse = acc;         
+
+      if (rec !== '*') {
+          count++;
+      }
+
+        if ((index%2 === 0) && (rec === '1')) {
+            if (expr[index + 1] === '0') {
+                // acc += '.';
+                morse = morse + '.'
+            } else {
+                 morse = morse + '-'
+            }
+          }
+      if (rec == '*') {
+        countStar = countStar + 1;
+      }
+
+      if (countStar === 10) {
+          word = word + ' ';
+          countStar = 0;
+        }
+    
+        if (count === 10) {
+          word = word + MORSE_TABLE[morse];
+            morse = '';
+           count = 0;
+          }
+      return morse;
+    }, '');
+    return word;
 }
 
 module.exports = {
